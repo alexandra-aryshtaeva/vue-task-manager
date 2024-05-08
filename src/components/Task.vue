54c5d5216d3b86d4809d42519a0b5f97d2a581a9
Alexandra Aryshtaeva <alexandra.maranga@gmail.com>
2024-05-08 11:47:13 +0100

    dividing the app into components

---
+++ src/components/Task.vue
@@ -0,0 +1,29 @@
+<script setup></script>
+
+<template>
+  <div class="wrapper">
+    <input type="text" />
+    <div class="task">Insert Task</div>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  background-color: aqua;
+  display: flex;
+  flex-direction: column;
+  margin: 1rem 1rem;
+}
+input {
+  margin-top: 1rem;
+  display: flex;
+}
+
+.task {
+  margin-top: 1rem;
+  border: solid black 1px;
+  border-radius: 4px;
+  text-align: center;
+  padding: 1rem;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "@/style.css";
+const app = createApp(App);
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+app.mount("#app");
